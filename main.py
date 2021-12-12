c2bb1891f127389f725cc7a693bf55ab83a01af0
issamoxix <issamhaidaoui@gmail.com>
2021-12-12 22:46:40 +0100

    no message

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from solution import crawler
 import os
 
-#loop thru files from the same derictory
-for i in os.listdir():
+
+if __name__ == "__main__":
+    # loop thru files from the same derictory
+    for i in os.listdir():
         if '.html' in i:
             print(i)
-def ChooseFile():
-    #enter the file name if it is in the same directory if not enter the path
-    file = input('Enter File name : ')
-    if not os.path.isfile(file):
-        return ChooseFile()
-    if '.html' not in file:
-        print('Enter html file !')
-        return ChooseFile()
-    return file
 
+    def ChooseFile():
+        # enter the file name if it is in the same directory if not enter the path
+        file = input('Enter File name : ')
+        if not os.path.isfile(file):
+            return ChooseFile()
+        if '.html' not in file:
+            print('Enter html file !')
+            return ChooseFile()
+        return file
 
-web = crawler(ChooseFile())
-web.getFooter()
-web.getBodyfooter()
-web.getBodyData()
-web.getHeaderData()
-web.DumpData()
+    web = crawler(ChooseFile())
+    web.getFooter()
+    web.getBodyfooter()
+    web.getBodyData()
+    web.getHeaderData()
+    web.DumpData()
 
-web.DumptoJson(web.DisplayData()[0])
+    web.DumptoJson(web.DisplayData()[0])
